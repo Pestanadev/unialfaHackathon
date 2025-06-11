33b4d41fed0dd3d3ae7e3341657e1c9643bf3d82
AntonioMMP <antoniomarcosmarianopereira@gmail.com>
2025-06-11 19:33:57 -0300

    Classes criadas, e começando a trabalhar com a api

---
+++ classes/ApiComNode.php
@@ -0,0 +1,37 @@
+<?php 
+
+class ApiNode
+{
+    private string $baseUrl;
+
+    public function __construct()
+    {
+        $this->baseUrl = 'http://localhost:3001';
+    }
+    public function getPosts()
+{
+    $url = $this->baseUrl . '/usuarios';
+
+    $curl = curl_init($url);
+    curl_setopt_array($curl, [
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_TIMEOUT => 10,
+        CURLOPT_HTTPHEADER => [
+            'Accept: application/json',
+            'User-Agent: PHP-Eventos-App'
+        ]
+    ]);
+
+    $response = curl_exec($curl);
+    curl_close($curl);
+
+    if (!$response) {
+        return [];
+    }
+
+    $data = json_decode($response, true);
+
+    return $data['usuarios'] ?? [];
+}
+
+}
