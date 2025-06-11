fa05208370a2a6ea588a61cb41b861f64a503cc5
Renan0204 <ro6680476@gmail.com>
2025-06-11 20:25:43 -0300

    Feito a parte de banner e organizando corres da paleta.

--- public/index.php
+++ public/index.php
@@ -16,12 +16,24 @@
     <div class="centralizado">
         <?php include '../templates/header.php'; ?>
 
-        <div class="card" style="width: 23rem;">
-            <img src="..." class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+        <div class="centralizado">
+            <?php include '../templates/header.php'; ?>
+
+            <div class="banner-container">
+                <div class="banner-text">
+                    <h1 class="banner-title">Descubra seu Próximo Evento</h1>
+                    <p class="banner-desc">Venha participar de uma experiência única! Garanta já sua inscrição.</p>
+                </div>
+                <img src="../style/img/banner1.png" alt="banner1" class="top-banner">
+            </div>
+
+            <div class="card" style="width: 23rem;">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
             </div>
         </div>
     </div>
