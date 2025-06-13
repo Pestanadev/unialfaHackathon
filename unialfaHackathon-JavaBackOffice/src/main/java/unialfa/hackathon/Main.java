7c29403e8ad62694e8fc079478f54a500cb692d1
Pestanadev <felipepestanadev@gmail.com>
2025-06-13 19:45:09 -0300

    CRUD finalizado

---
+++ unialfaHackathon-JavaBackOffice/src/main/java/unialfa/hackathon/Main.java
@@ -0,0 +1,29 @@
+package unialfa.hackathon;
+
+import unialfa.hackathon.gui.EventoGui;
+import unialfa.hackathon.service.EventoService;
+import javax.swing.*;
+
+/*
+ * Classe principal para iniciar a aplicação.
+ * Utiliza o Swing para criar a interface gráfica.
+ */
+// Autor: [Felipe Pestana]
+
+public class Main {
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(Main::executar);
+    }
+
+    /*
+     * Método para inicializar a interface gráfica da aplicação.
+     * Cria uma instância de EventoGui e a torna visível.
+    */
+
+    private static void executar() {
+        var EventoGui = new EventoGui(new EventoService());
+        EventoGui.setVisible(true);
+    }
+}
+
